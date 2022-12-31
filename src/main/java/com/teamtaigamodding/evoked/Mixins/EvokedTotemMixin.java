c6dd70e2ee80a099729f453a2f14e42f5e40d1cd
TheLegitDolt <asphaltpotatofy@gmail.com>
2022-12-30 22:12:35 -0800

    stuff

--- src/main/java/com/teamtaigamodding/evoked/Mixins/EvokedTotemMixin.java
+++ src/main/java/com/teamtaigamodding/evoked/Mixins/EvokedTotemMixin.java
@@ -36,14 +36,19 @@ public abstract class EvokedTotemMixin extends Entity {
     @Inject(at = @At("TAIL"), method = "Lnet/minecraft/world/entity/LivingEntity;checkTotemDeathProtection(Lnet/minecraft/world/damagesource/DamageSource;)Z", cancellable = true)
     private void CheckTotemDeathProtection(DamageSource DamageSource, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity LE = (LivingEntity) (Object) this;
-
-        if (LE.getHealth() == 1.0f && LE.getEffect(MobEffects.ABSORPTION) != null) EvokedTickHandler.scheduleAsyncTask(new GiveTotemTask(LE), 2130, TimeUnit.MILLISECONDS);
+        boolean isReal = LE.getEffect(MobEffects.ABSORPTION).getAmplifier() == 1
+                && LE.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() == 1
+                && LE.getEffect(MobEffects.FIRE_RESISTANCE).getAmplifier() == 0
+                && LE.getEffect(MobEffects.ABSORPTION).getDuration() == 100 &&
+                LE.getEffect(MobEffects.FIRE_RESISTANCE).getDuration()  == 800;
+        if (LE.getHealth() == 1.0f && isReal) EvokedTickHandler.scheduleAsyncTask(new GiveTotemTask(LE), 2130, TimeUnit.MILLISECONDS);
     }
 
     @Inject(at = @At("HEAD"), method = "Lnet/minecraft/world/entity/LivingEntity;checkTotemDeathProtection(Lnet/minecraft/world/damagesource/DamageSource;)Z", cancellable = true)
     private void CheckTotemDeathProtectionH(DamageSource DamageSource, CallbackInfoReturnable<Boolean> cir) {
         if (EvokedConfig.Common.COMMON.TotemsWorkInVoid.get()) {
             LivingEntity LE = (LivingEntity) (Object) this;
+
             if (LE.getY() < -64 && DamageSource.equals(DamageSource.OUT_OF_WORLD)) {
                 ItemStack itemstack = null;
                 for (InteractionHand interactionhand : InteractionHand.values()) {
